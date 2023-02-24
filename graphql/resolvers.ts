de61b8376eccdc1a7af3436d392968196b7a31d9
mhNirob <smh.nirob@gmail.com>
2023-02-24 13:35:01 +0600

    add graphql api using apollo server

---
+++ graphql/resolvers.ts
@@ -0,0 +1,23 @@
+export const resolvers = {
+    Query: {
+        links: () => [
+            {
+                category: "Open Source",
+                description: "Fullstack React framework",
+                id: "1",
+                imageUrl: "https://github.com/vercel.png",
+                title: "Next.js",
+                url: "https://nextjs.org",
+            },
+            {
+                category: "Open Source",
+                description: "Next Generation ORM for TypeScript and JavaScript",
+                id: "2",
+                imageUrl: "https://github.com/prisma.png",
+
+                title: "Prisma",
+                url: "https://prisma.io",
+            },
+        ]
+    }
+}
